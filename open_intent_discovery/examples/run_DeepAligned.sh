9285de7fb05d4155507c2e2cf384af93d82daf1d
padeck <patrick.a.deck@gmail.com>
2025-01-30 21:16:19 +0100

    new results

--- open_intent_discovery/examples/run_DeepAligned.sh
+++ open_intent_discovery/examples/run_DeepAligned.sh
@@ -1,11 +1,11 @@
 #!/usr/bin bash
 
 
-for dataset in 'banking' #'clinc'
+for dataset in 'labeled_5' #'clinc'
 do
-    for known_cls_ratio in 0.25
+    for known_cls_ratio in 0.25 0.5 0.75 #0.75 0.9
     do
-        for seed in 0 #1 2 3 4 5 6 7 8 9
+        for seed in 0 
         do 
             python run.py \
             --dataset $dataset \
